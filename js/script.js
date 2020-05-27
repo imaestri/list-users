2f2c1d168c5c2507b0e164378d698f7c06b85893
Isaque Maestri <isaquemaestri01@gmail.com>
2020-05-27 20:57:41 -0300

    refatorando o código

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // declarando as variáveis globais
 
-let userCount = 0;
 let totalAge = 0;
 let ageAvg = 0;
 let countGenderM = 0;
@@ -8,17 +7,16 @@ let countGenderF = 0;
 let mainInput = null;
 let cardUsers = null;
 let cardStatistics = null;
-let btnFunction = null;
+// let btnFunction = null;
 
 let allUsers = [];
 let userAdd = [];
-let allStatisticsUser = [];
 
 window.addEventListener("load", () => {
   mainInput = document.getElementById("mainInput");
   cardUsers = document.getElementById("cardUsers");
   cardStatistics = document.getElementById("cardStatistics");
-  btnFunction = document.getElementById("buttonSearch");
+//   btnFunction = document.getElementById("buttonSearch");
 
   render();
   fetchUsers();
@@ -43,6 +41,7 @@ async function fetchUsers() {
 function render() {
   waitForm();
   getInput();
+  renderlistUsers();
 }
 
 function waitForm() {
@@ -63,7 +62,94 @@ function getInput() {
         return a.name.localeCompare(b.name);
       });
     // console.log(userAdd);
-    // userCount = userAdd.length;
+   
     // console.log(userCount);
+
+    renderlistUsers();
+    renderListStatistics()
+    addListStatistics();
   });
+  
+}
+
+
+function addListStatistics() {
+  totalAge = userAdd.reduce((accumulator, current) => {
+    return accumulator + current.age;
+  }, 0);
+  // console.log(totalAge);
+
+  ageAvg =
+    userAdd.reduce((accumulator, current) => {
+      return accumulator + current.age;
+    }, 0) / userCount;
+  // console.log(ageAvg);
+
+  totalGenderM = userAdd.filter((users) => {
+    return users.gender === "male";
+  }).length;
+
+  totalGenderF = userAdd.filter((users) => {
+    return users.gender === "female";
+  }).length;
+
+  // console.log(totalGenderM);
+  // console.log(totalGenderF);
+}
+
+
+
+function renderlistUsers() {
+  let userCount = userAdd.length;  
+
+  let usersListHTML = `
+    <div>
+    <p id="count-users" >${userCount} usuário(s) encontrado(s)</p>
+    <br>
+    </div>
+  `;
+
+  userAdd.forEach((users) => {
+    const { name, picture, age, gender } = users;
+
+    let userListHTML = `
+      <div class = "cardUsers">
+      
+        <div>
+          <img src="${picture}" alt="${name}">
+          <ul>
+            <li> ${name}, ${age}, ${gender}. </li>
+          </ul>
+        </div>
+      </div>
+    `;
+
+    usersListHTML += userListHTML;
+  });
+
+  cardUsers.innerHTML = usersListHTML;
+}
+
+function renderListStatistics(){
+    let usersStatisticsHTML = `
+        <div>
+        <p id="users-statistics" > Estatísticas </p>
+        <br>
+        </div>
+  `;
+
+  userAdd.forEach((users)=>{
+      let userStatisticsHTML = `
+      <div class = "cardStatistics">
+        <ul>
+            <li>Soma das idades: ${totalAge} </li>
+            <li>Média das idades: ${ageAvg} </li>
+            <li>Sexo masculino: ${totalGenderM} </li>
+            <li>Sexo feminino: ${totalGenderF}  </li>
+        </ul>
+      </div>
+      `;
+      usersStatisticsHTML +=  userStatisticsHTML;
+  });
+  cardStatistics.innerHTML = usersStatisticsHTML;
 }
